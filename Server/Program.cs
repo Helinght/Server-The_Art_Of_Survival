0729c3d5d9f9ae1fb15f8a30ad2453fb6a36ee31
fel4k <92309143+fel4k@users.noreply.github.com>
2023-05-03 22:15:41 +0700

    Add function (Add code)

--- Server/Program.cs
+++ Server/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
-namespace Server
+namespace Program
+
+//Console.WriteLine("[",DateTime.Now,"]","(","INFO",")","");
+/* https://russianblogs.com/article/30793054479/ */
 {
-    class Server
-
+    static void Main(string[] args)
     {
-        Public int Port:
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        var Port = 6324;
+        IPEndPoint IpAndPort = new IPEndPoint(ipAddress, Port);
+
+        serverSocket.Bind(IpAndPort);
+        serverSocket.Listen(0);
+        Console.WriteLine("[",DateTime.Now,"]","(","INFO",")","Server start!");
+        Console.WriteLine("[", DateTime.Now, "]", "(", "INFO", ")", "Ip - ",ipAddress);
+        Console.WriteLine("[", DateTime.Now, "]", "(", "INFO", ")", "Port - ",Port);
+        serverSocket.BeginAccept(AcceptCallBack, serverSocket);
     }
 }
